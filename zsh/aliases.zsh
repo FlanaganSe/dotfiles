ae912590314f3e932002f0648423b5fbf76b78ce
Sean Flanagan <sean.flanagan@trajectorservices.com>
2025-10-01 11:02:23 -0400

    feat: add additional aliases for git branch commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
 #Git aliases
 alias glol='--pretty=format:"%C(auto)%h %ad | %s%d [%an]%C(reset)" --graph --date=short'
+alias gbl5="git for-each-ref --sort=-committerdate --count=5 --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:short) (%(committerdate:relative))%(color:reset) - %(color:blue)%(authorname)%(color:reset) - %(contents:subject)' refs/heads/"
+alias gbl10="git for-each-ref --sort=-committerdate --count=10 --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:short) (%(committerdate:relative))%(color:reset) - %(color:blue)%(authorname)%(color:reset) - %(contents:subject)' refs/heads/"
+alias gbl25="git for-each-ref --sort=-committerdate --count=25 --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:short) (%(committerdate:relative))%(color:reset) - %(color:blue)%(authorname)%(color:reset) - %(contents:subject)' refs/heads/"
 alias ga='git add'
 alias gs='nocorrect git status'
 alias gc='git commit '
